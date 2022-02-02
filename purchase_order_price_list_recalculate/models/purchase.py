be6d020c03d2069f2faf1613220728021239b9d0
habbaba <34061969+habbaba@users.noreply.github.com>
2022-02-02 22:35:02 +0300

    Update purchase.py

--- purchase_order_price_list_recalculate/models/purchase.py
+++ purchase_order_price_list_recalculate/models/purchase.py
@@ -8,6 +8,6 @@ def _onchange_price_recalculation(self):
         res = super(PurchaseOrderLine, self)._onchange_requisition_id()
         if self.order_id.requisition_id != order_id.requisition_id:
             for line in self.order_id.requisition_id.line_ids.filtered(lambda l: l.product_id == self.product_id):
-                self.price_unit = line.product_uom_id._compute_price(
-                        line.price_unit, self.product_uom)
+                self.price_unit = line.product_id._compute_price(
+                        line.price_unit)
         return res
