ffb92fe8d6690141153f18a0389b44e8601cc678
habbaba <34061969+habbaba@users.noreply.github.com>
2022-02-02 23:41:44 +0300

    Update purchase.py

--- purchase_order_price_list_recalculate/models/purchase.py
+++ purchase_order_price_list_recalculate/models/purchase.py
@@ -4,8 +4,8 @@
 from odoo import api, fields, models
 
     @api.onchange('requisition_id', 'product_qty', 'product_uom')
-    def _onchange_quantity(self):
-        res = super(PurchaseOrderLine, self)._onchange_quantity()
+    def _onchange_requisition_id(self):
+        res = super(PurchaseOrderLine, self)._onchange_requisition_id()
         if self.order_id.requisition_id:
             for line in self.order_id.requisition_id.line_ids.filtered(lambda l: l.product_id == self.product_id):
                 if line.product_uom_id = self.product_uom:
