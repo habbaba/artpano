622d207244f1834fb6945fd75d2061b9693f5d7c
habbaba <34061969+habbaba@users.noreply.github.com>
2022-02-02 19:06:15 +0300

    Add files via upload

---
+++ purchase_order_price_list_recalculate/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2021 M.Kamal Habbaba <habbaba@mkhabbaba.com>
+# License LGPL-3 - See http://www.gnu.org/licenses/Lgpl-3.0.html
+
+from . import models
---
+++ purchase_order_price_list_recalculate/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2021 M.Kamal Habbaba <habbaba@mkhabbaba.com>
+# License LGPL-3 - See http://www.gnu.org/licenses/Lgpl-3.0.html
+
+
+from . import purchase
