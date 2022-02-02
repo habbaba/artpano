407f95c4e606ed20f6da80fdcc9dc0eccf565a0e
habbaba <34061969+habbaba@users.noreply.github.com>
2022-02-02 21:21:53 +0300

    Update purchase.py

--- purchase_order_price_list_recalculate/models/purchase.py
+++ purchase_order_price_list_recalculate/models/purchase.py
@@ -3,21 +3,15 @@
 
 from odoo import api, fields, models
 
-class PurchaseOrderRecalculation(models.Model):
-    _inherit = 'purchase.order'
-
-    @api.onchange('requisition_id', 'partner_id')
-    def _onchange_price_recalculation(self):
-        """this method use to show the product price according to the sales order price list"""
-        if self.partner_id and self.requisition_id:
-            for line in self.order_line:
-                product = line.product_id.with_context(
-                    lang=line.order_id.partner_id.lang,
-                    partner=line.order_id.partner_id,
-                    quantity=line.product_uom_qty,
-                    date=line.order_id.date_order,
-                    pricelist=line.order_id.requisition_id.id,
-                    uom=line.product_uom.id,
-                )
-                line.price_unit = self.env['account.tax']._fix_tax_included_price_company(
-                    line._get_display_price(product), product.taxes_id, line.tax_id, line.company_id)
+@api.onchange('requisition_id')
+     def _onchange_price_recalculation(self):
+        res = super(PurchaseOrderLine, self)._onchange_quantity()
+        if self.order_id.requisition_id:
+            for line in self.order_id.requisition_id.line_ids.filtered(lambda l: l.product_id == self.product_id):
+                if line.product_uom_id != self.product_uom:
+                    self.price_unit = line.product_uom_id._compute_price(
+                        line.price_unit, self.product_uom)
+                else:
+                    self.price_unit = line.price_unit
+                break
+        return res               
