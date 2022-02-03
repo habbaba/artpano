51f2f2f7de988be6b5c57f293db1f34ec95a8a46
habbaba <34061969+habbaba@users.noreply.github.com>
2022-02-03 16:20:20 +0300

    Update purchase.py

--- purchase_order_price_list_recalculate/models/purchase.py
+++ purchase_order_price_list_recalculate/models/purchase.py
@@ -6,26 +6,15 @@ from odoo import api, fields, models
 class PurchaseOrderRecalculation(models.Model):
     _inherit = 'purchase.order'
 
-    @api.onchange('requisition_id', 'partner_id')
-    def _onchange_price_recalculation(self):
-        """this method use to show the product price according to the sales order price list"""
-        if self.partner_id and self.requisition_id:
-            for line in self.order_line:
-                product = line.product_id.with_context(
-                    lang=line.order_id.partner_id.lang,
-                    partner=line.order_id.partner_id,
-                    quantity=line.product_uom_qty,
-                    date=line.order_id.date_order,
-                    pricelist=line.order_id.requisition_id.id,
-                    uom=line.product_uom.id,
-																		   
-                )
-
-    @api.onchange('requisition_id', 'partner_id')
-    def _onchange_discount_recalculation(self):
-        """this method is mainly used for the discount price calculate according to the price list"""
-        if self.partner_id and self.requisition_id:
-            for line in self.order_line:
-                if not (line.product_id and line.product_uom and
-                        line.order_id.partner_id and line.order_id.requisition_id):
-                    return
+    @api.onchange('product_qty', 'product_uom')
+    def _onchange_quantity(self):
+        res = super(PurchaseOrderLine, self)._onchange_quantity()
+        if self.order_id.requisition_id:
+            for line in self.order_id.requisition_id.line_ids.filtered(lambda l: l.product_id == self.product_id):
+                if line.product_uom_id != self.product_uom:
+                    self.price_unit = line.product_uom_id._compute_price(
+                        line.price_unit, self.product_uom)
+                else:
+                    self.price_unit = line.price_unit
+                break
+        return res
